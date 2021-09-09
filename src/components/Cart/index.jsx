1a0b3f577507a5a9bc105afb44939f1b4ccd75d9
glen1986 <escobar.glen@gmail.com>
2021-09-09 18:39:54 -0300

    hamburgCart

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,12 @@
 import './styles.css'
-const Cart = ({currentSale})=> {
+const Cart = ({item,currentSale})=> {
   return(
   <div className="cart">
-    {currentSale.map(item=><div className="cartId">{item.id}</div>)}
-    {currentSale.map(item=><div className="cartName">{item.name}</div>)}
-    {currentSale.map(item=><div className="cartCategory">{item.category}</div>)}
-    {currentSale.map(item=><div className="cartPrice">{item.price}</div>)}
+    <div className="cartId">{item.id}</div>
+    <div className="cartName">{item.name}</div>
+    <div className="cartCategory">{item.category}</div>
+    <div className="cartPrice">{item.price}</div>
+    <button onClick={()=>console.log("buy")}></button>
   </div>
   )
 }
