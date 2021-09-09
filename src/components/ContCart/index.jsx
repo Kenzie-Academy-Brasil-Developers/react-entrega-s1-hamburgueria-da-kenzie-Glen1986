6129a81874115f441acc715e8199ebf0e9fe6ffc
glen1986 <escobar.glen@gmail.com>
2021-09-09 19:11:31 -0300

    hamburgCartCarrinho

--- src/components/ContCart/index.jsx
+++ src/components/ContCart/index.jsx
@@ -1,15 +1,16 @@
+import './styles.css'
 import Cart from '../Cart'
 
 const ContCart =({currentSale})=>{
   return(
-    <>
+    <section className="carrinho">
       <h2>Carrinho</h2>
       <div className="container">
         {
           currentSale.map(item => <Cart key={item.id} item={item}/>)
         }
       </div>
-    </>
+    </section>
   )
 }
 export default ContCart;
